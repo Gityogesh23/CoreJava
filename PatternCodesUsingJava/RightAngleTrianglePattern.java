4de3f0dddea9f7d4d23d8bdc4abd54845e1f0161
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-07-20 20:48:45 +0530

    final commit

---
+++ PatternCodesUsingJava/RightAngleTrianglePattern.java
@@ -0,0 +1,23 @@
+package PatternCodesUsingJava;
+/*
+  to print RightAngleTriangle Pattern:
+          *
+        * *
+      * * *
+    * * * *
+ */
+public class RightAngleTrianglePattern {
+	public static void main(String args[]) {
+		  int row=6;
+		 for(int i=0;i<row;i++) {
+
+			for(int j=0;j<=i;j++) {
+
+				System.out.print("* ");
+			}
+
+			System.out.println(" ");
+		 }
+	}
+	
+}
