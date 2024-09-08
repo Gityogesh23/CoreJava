c6cc840f5e692d660ab7e8988ddaf6f432902996
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-09-08 17:17:49 +0530

    Second Commit

--- GreatestNum/GreatestNum.java
+++ GreatestNum/GreatestNum.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
-
-
+/*
+ 1)by using ternary operator=>
+ (condition)?(if true:Action):(if false:Action)
+ ex.result=num1>num2 ? num1:num2
+ 2)by using if-else=>if not given below=>its if(num1>num2)=>then num1 else num2 is greater 
+ 
+ 3)math.max();
+ */
+/*
 public class GreatestNum {
     
     public static void main(String[] args) {
@@ -16,6 +23,22 @@ public class GreatestNum {
         else{
         System.out.println(result=num1>num2 ? num1:num2);
         System.out.println("Greatest Number Become: "+result);
+        }
+    }
+}
+*/
+class GreatestNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number num1 :");
+        int num1=sc.nextInt();
+        System.out.println("Enter the Number num2 :");
+        int num2=sc.nextInt();
+        if(num1==num2){
+            System.out.println("numbers are equal");
+        }
+        else{
+        System.out.println(Math.max(num1,num2)+" "+ "is greater");
         }
     }
 }
