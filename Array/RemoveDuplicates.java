809efdcd059d559bf06031352dd077a778972c5f
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-09-13 00:10:16 +0530

    first commit

--- Array/RemoveDuplicates.java
+++ Array/RemoveDuplicates.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 public class RemoveDuplicates {
     public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
@@ -21,6 +24,16 @@ public class RemoveDuplicates {
         }
         sc.close();//to close the open resource.
     }
-     
+    //1) =>by using collection=> Set=>now to remove duplicate elements from the array.
+        Set<Integer>s1=new HashSet<>(); //hashset is the implementation clas of set interface.
+        System.out.println("to duplicate elements inside array: ");
+        s1.add(10);
+        s1.add(20);
+        s1.add(30);
+        s1.add(10);
+        System.out.println(s1.size());
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i]);
+        }
     }
 }
