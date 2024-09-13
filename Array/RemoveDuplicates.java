02548f7696db3b91a63c1ff31114f3eab80d8296
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-09-13 22:43:34 +0530

    third commit

--- Array/RemoveDuplicates.java
+++ Array/RemoveDuplicates.java
@@ -2,34 +2,43 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.HashSet;
 
-public class RemoveDuplicates {
+public class RemoveDuplicates{
     public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       System.out.println("enter the size of an array: ");
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the size of an array: ");
        
-       int n=sc.nextInt(); 
-       int[] arr=new int[n];//initializing array
-       if(n<0){
-        System.out.println("size should not be negative: ");
-       }
-       else{
-        //to give an input as elements to array.
-        System.out.println("Enter the array elements");
-        for(int i=0;i<n;i++){
-          arr[i]=sc.nextInt();
-        }
-        System.out.print(" " +"Array elements are: ");
-        Set<Integer>hash_Set = new HashSet<>();
-        for (int i=0; i<n; i++){
-        hash_Set.add(arr[i]);
-        System.out.print(" "+hash_Set);
-        }
-        System.out.println();
-    
-    //1) =>by using collection=> Set=>now to remove duplicate elements from the array.
+       int n = sc.nextInt(); 
+       if (n < 0) {
+           System.out.println("Size should not be negative.");
+       } else {
+           int[] arr = new int[n]; // initializing array
 
-    System.out.println("Array after removing duplicates: " + hash_Set);
-    }
-    sc.close();//to close the open resource.
+           // Input array elements
+           System.out.println("Enter the array elements:");
+           for (int i = 0; i < n; i++) {
+               arr[i] = sc.nextInt();
+           }
+
+           // Print array elements
+           System.out.print("Array elements are: ");
+           for (int i = 0; i < n; i++) {
+               System.out.print(" " + arr[i]);
+           }
+           System.out.println();
+
+           // Use Set to remove duplicate elements
+           Set<Integer> uniqueElements = new HashSet<>();
+           for (int i = 0; i < n; i++) {
+               uniqueElements.add(arr[i]);
+           }
+
+           // Print the unique elements (duplicates removed)
+           System.out.print("Array after removing duplicates: ");
+           for (int element : uniqueElements) {
+               System.out.print(" " + element);
+           }
+           System.out.println();
+       }
+       sc.close(); // Close the scanner
     }
 }
