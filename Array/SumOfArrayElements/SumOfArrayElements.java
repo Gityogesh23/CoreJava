e3081bbfae7027ae05bc90b9523316ea243cb931
Gityogesh23 <yogeshpatil1844@gmail.com>
2024-09-15 22:23:24 +0530

    second commit

---
+++ Array/SumOfArrayElements/SumOfArrayElements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SumOfArrayElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of an array: ");
+        
+        int n = sc.nextInt(); 
+        if (n < 1) {
+            System.out.println("Size should not be negative.");
+        } 
+        else {
+            int[] arr = new int[n]; // initializing array
+ 
+            // Input array elements
+            System.out.println("Enter the array elements:");
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+ 
+            // Print array elements
+            System.out.print("Array elements are: ");
+            for (int i = 0; i < n; i++) {
+                System.out.print(" " + arr[i]);
+            }
+            System.out.println();
+        }
+        
+    }
+}
