70a31afae6bcc5b025a5ab4abb6c13f5cc306266
Yogesh Patil <yogeshpatil1844@example.com>
2025-02-25 14:31:41 +0530

    optimised factorial program

--- Factorialnumber/Factorial.java
+++ Factorialnumber/Factorial.java
@@ -1,7 +1,8 @@
-package Factorialnumber;
+//package Factorialnumber;
 
 /*Factorial using Recursion :
-Logic:
+Logic:1)
+
 5!=5*4*3*2*1=120;
 6!=720;
 7!=5040 and so on.
@@ -10,6 +11,9 @@ Logic:
 but what if negative value is given by user then==> go to line number 28:=>now my program is perfectly handled
 
 */
+
+ 
+/*
 import java.util.Scanner;
  class Factorial {
      static int fact(int num){
@@ -35,3 +39,49 @@ import java.util.Scanner;
         System.out.println("factorial of " +" "+ count +" is :" +factorial);
         }
 }
+*/
+/* 
+ //find Factorial==>Logic 2
+import java.util.Scanner;
+import java.math.BigInteger;
+class Test{
+    public static BigInteger fact(BigInteger num){
+        if(num.equals(BigInteger.ZERO))
+        return BigInteger.ONE;
+        else
+        return num.multiply(fact(num.subtract(BigInteger.ONE)));
+    }
+}
+public class Factorial{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+          System.out.println("Enter the Number: ");
+          BigInteger num=new BigInteger(sc.next());
+          BigInteger factorialNum= Test.fact(num);
+           System.out.println("Factorial of : "+num+ "is "+factorialNum );
+           sc.close();
+    }
+}
+*/
+//Logic 3)
+import java.util.Scanner;
+ class Test{
+        public static int fact(int number){
+            //ternary operator
+            return(number==0 || number==1) ? 1 : number *fact(number-1);
+        } 
+ }
+
+ class Factorial
+ {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number");
+        int num=sc.nextInt();
+        if(num<0){
+            System.out.println("Enter positive number only");
+        }
+        int result=Test.fact(num);
+        System.out.println("Factorial of number : "+ num +" is "+result);
+    }
+ }
