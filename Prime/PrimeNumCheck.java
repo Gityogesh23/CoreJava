7bf533196b1ba65783ffa35039a41b07cec15052
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-02 15:53:50 +0530

    First commit of the day

--- Prime/PrimeNumCheck.java
+++ Prime/PrimeNumCheck.java
@@ -1,24 +1,34 @@
+/*
+prime number=>number>=2 and for(int i=2;;i<=Math.sqrt(number);i++)  
+condition for prime number=> smaller prime number is 2 and if(number%2!=0)
+we use logic for into the for loop=>for(int i=2;i<=Math.sqrt(number);i++{})
+*/
 import java.util.Scanner;
-
-public class  PrimeNumCheck {
-    public static void checkIfPrime(int num){
-            if(num%2==0){
-                System.out.println("number is prime");
+ class PrimeNumCheck{
+public static void checkPrime(int number){
+            if(number==2){
+                System.out.println("Is Prime");
+                return;
             }
-          else{
-            System.out.println("number is not prime");
-          }
-    }
+            for(int i=2;i<=Math.sqrt(number);i++){ 
+                //eg 37=>6.08=>2,3,4,5,6  =>37 is not divisible by these numbers.
+                //37 is only divisible by 37 and 1 itself hence its Prime Number.
+               if(number%2==0) {
+                System.out.println("Not  Prime");
+                return ;
+               }
+            }
+            System.out.println("Is Prime");
+            
+}
+
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter the number: ");
-        int num= sc.nextInt();
-        if (num<1){
-            System.out.println("plz enter the positive number : ");
-
-        }
-        else{
-        checkIfPrime(num);
-        }
+     System.out.println("Enter the Number: ");
+     int num=sc.nextInt();
+     if(num<=0){
+        System.out.println("Enter the valid input number ");
+     }
+     checkPrime(num);
     }
-}
+ }
