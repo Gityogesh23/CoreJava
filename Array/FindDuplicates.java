84a9b7f9e563f44edaf445d50acc059a9070dc56
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-08 19:36:08 +0530

    commit second

--- Array/FindDuplicates.java
+++ Array/FindDuplicates.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
 
 public class FindDuplicates {
-    Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
    // int[] arr=new int[5];//declaring and initializing array in java which takes 5 elements.
-int[] array_1=new int[]{1,2,4,5,4,1};
+    //int[] array_1=new int[]{1,2,4,5,4,1};
+//by custome input
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    System.out.println("Enter the elements inside an array: ");
+   // int size; //local variable hence no need of initialization
+    int[]array_1=new int[size];
+    for(int i=0;i<size;i++){
+     array_1[i]=sc.nextInt();
+
+    }
+    System.out.print("Array elements are: ");
+    for (int i = 0; i < size; i++) {
+        System.out.print(" " + array_1[i]);
+    }
+sc.close();
+   }
 }
