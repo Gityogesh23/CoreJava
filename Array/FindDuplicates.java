6f77fa9e5b9cf1f4e948f72d4619a7f534f019a2
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-08 19:10:05 +0530

    commit

---
+++ Array/FindDuplicates.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class FindDuplicates {
+    Scanner sc=new Scanner(System.in);
+    int[] arr=new int[5];//declaring and initializing array in java which takes 5 elements.
+}
