a691f6220be12fb38cab3dee91f13cd6d5b564b2
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-10 21:15:12 +0530

    commit third

--- Array/ArrayReverse.java
+++ Array/ArrayReverse.java
@@ -1,4 +1,20 @@
-
+import java.util.Scanner;
 public class ArrayReverse {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array : ");
+        int size=sc.nextInt();
+        int array[]=new int[size];
+        for(int i=0;i<size;i++){
+            array[i]=sc.nextInt();
+
+        }
+        System.out.print("Array elements are: ");
+        for(int i=0;i<size;i++){
+            System.out.println("array elements are: "+array[i]);
+        }
+        for(int i=array.length-1;i>=0;i++){
+            System.out.print("Reverse array elements are: array[i]);
+        }
+    }
 }
