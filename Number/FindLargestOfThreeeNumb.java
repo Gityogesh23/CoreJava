84c04f7172ff60bf3cb5f17943765a4283cbeb8c
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-17 20:47:45 +0530

    commit change

--- Number/FindLargestOfThreeeNumb.java
+++ Number/FindLargestOfThreeeNumb.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 
 public class FindLargestOfThreeeNumb {
+    public static void main(String[] args){
     int temp;
     Scanner sc=new Scanner(System.in);
     System.out.println("enter the first number: ");
@@ -14,4 +15,5 @@ public class FindLargestOfThreeeNumb {
     System.out.println("Greater Number Become: ");
     temp = num3 > (num1 > num2 ? num1 : num2) ? num3 : ((num1 > num2) ? num1 : num2);
     System.out.println("The largest number is: "+temp); 
+   }
 }
