a59286ba8acf162d01b35c57a3edf882531da64c
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-19 21:26:00 +0530

    changes made

--- CoreJavaStringIVRelated/CountCharactersInString.java
+++ CoreJavaStringIVRelated/CountCharactersInString.java
@@ -2,11 +2,18 @@ import java.util.Scanner;
 
 class CountCharactersInString{
 public static void CountCharsInString(String str) {
-   
-    
+    String str1=str.replaceAll("[^A-Z a-z]"," ");
+   int count=0;
+  for(int i=0;i<str1.length();i++){
+       if(str1.charAt(i)!=' '){   // Exclude spaces, count only characters
+        count++;
+       }
+    }
+    System.out.println("Number of characters in the given string excluding space are: " +count);
 }
+
 public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);\
+    Scanner sc=new Scanner(System.in);
     System.out.println("Enter the String ");
     try{
     String str=sc.nextLine();
@@ -18,5 +25,6 @@ System.out.println(" enter proper input in string/characters format");
 }
 finally{
     sc.close();
-}
+      }
+   }
 }
