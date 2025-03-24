34098f1a7840fb5e2343e8c140f27c43fee7d657
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-24 12:03:59 +0530

    commit

--- PatternCodesUsingJava/RightAlignedPyramid.java
+++ PatternCodesUsingJava/RightAlignedPyramid.java
@@ -11,8 +11,11 @@ public class RightAlignedPyramid {
     public static void main(String[] args) {
         int rows=6;
         for(int i=0;i<rows;i++){
-           for(int j=2*(rows-i);j>=0;j--){
-
+           for(int j=2*(rows-i);j>=0;j--){  //instead of this line we can also write
+      //for(int j=0;j<(rows-i-1);j++{}) and next
+      //must give two spaces in print of next statement for proper output
+      //otherwise it wil gives pyramid only or triangle shape
+      //refer Triangle.java
             System.out.print(" "); //to print space between two stars//imp line
            }
            for(int j=0;j<=i;j++){
