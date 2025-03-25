1b005f8fa9c44b7ee74fe252834a5fd24d3662fd
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-25 20:16:37 +0530

    commit

---
+++ CoreJavaStringIVRelated/StringRemovingSpaces.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class StringRemovingSpaces {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        try{
+        String str=sc.nextLine();
+        String str1=str.replaceAll("\\s+",""); //removing spaces
+        System.out.println("String after removing all the white spaces : " + str1);    
+        }
+        catch(Exception e){
+            System.out.println("input ,mismatch Exception");
+           
+            sc.close();
+        }
+    }
+}
