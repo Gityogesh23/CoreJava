b6fc4dcf67183e8d99d91eb9e5bc59f7dea01117
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-26 20:51:05 +0530

    commit

---
+++ Array/SortArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class SortArray {
+public static int[] sortArray(int[]originalArray){
+    int[]SortedArray=originalArray;
+    Arrays.sort(SortedArray);
+    //to print sorted array
+  
+    return SortedArray;
+}
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of an array: ");
+        System.out.println();
+        int size=sc.nextInt();
+        System.out.println("Insert an elements inside array: ");
+        int[] originalArray=new int[size];
+        for(int i=0;i<size;i++){
+            originalArray[i] =sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for(int i=0;i<originalArray.length;i++){
+            System.out.print(" "+originalArray[i]);
+        }
+        System.out.println();
+       int[] sortedArray= sortArray(originalArray);
+       for(int i=0;i<originalArray.length;i++){
+        System.out.print("  "+sortedArray[i]);
+      }
+    }
+}
--- CoreJavaStringIVRelated/StringRemovingSpaces.java
+++ CoreJavaStringIVRelated/StringRemovingSpaces.java
@@ -4,6 +4,7 @@ public class StringRemovingSpaces {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         try{
+            System.out.println("Enter the String With spaces: ");
         String str=sc.nextLine();
         String str1=str.replaceAll("\\s+",""); //removing spaces
         System.out.println("String after removing all the white spaces : " + str1);    
