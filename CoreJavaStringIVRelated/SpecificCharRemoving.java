4d204343de3297f502d0f932496a7617049ea8fc
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-27 20:39:26 +0530

    final commit

--- CoreJavaStringIVRelated/SpecificCharRemoving.java
+++ CoreJavaStringIVRelated/SpecificCharRemoving.java
@@ -1,8 +1,26 @@
+
 import java.util.Scanner;
-public class SpecificCharRemoving {
+
+public class  SpecificCharRemoving {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter The String: ");
-        
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter the string: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter the character to remove: ");
+        char chToRemove = sc.next().charAt(0);
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (ch != chToRemove) {
+                sb.append(ch);
+            }
+        }
+
+        System.out.println("Modified String: " + sb.toString());
+
+        sc.close();
     }
 }
