2cc4f449afc6159667b3db4cf9965c8dc52b74db
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-27 20:13:40 +0530

    firstcommit

---
+++ CoreJavaStringIVRelated/SpecificCharRemoving.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class SpecificCharRemoving {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The String: ");
+        
+    }
+}
