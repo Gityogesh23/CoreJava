1d91691c353820500b06394cffe99f7bd6d2034a
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-29 20:25:19 +0530

    commit changes

--- CoreJavaStringIVRelated/SwapWithoutTemp.java
+++ CoreJavaStringIVRelated/SwapWithoutTemp.java
@@ -2,12 +2,20 @@
 import java.util.Scanner;
 public class SwapWithoutTemp {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in){
-            System.out.println("Enter the String");
+        Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the First String");
             String str1=sc.nextLine();
-            sc.next();
-            String str2=sc.nextline();
-            
+           
+            System.out.println("Enter the Second String");
+            String str2=sc.nextLine();
+            System.out.println("Before swap: " + str1 + " " + str2);
+            str1=str1+str2;//appending both string and combinely become string one
+            //sop is just for better understatnding 
+            System.out.println("after appending str1 becomes:"+str1); 
+            str2=str1.substring(0,str1.length()-str2.length());
+
+            str1=str1.substring(str2.length());
+
+            System.out.println("After swap: " +str1 + " " + str2);
         }
     }
-}
