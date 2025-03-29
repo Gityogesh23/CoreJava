e180c7f32893af21d0bbe31ee0d73f1f57c4e27b
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-03-29 20:13:12 +0530

    Commit

---
+++ CoreJavaStringIVRelated/SwapWithoutTemp.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+public class SwapWithoutTemp {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in){
+            System.out.println("Enter the String");
+            String str1=sc.nextLine();
+            sc.next();
+            String str2=sc.nextline();
+            
+        }
+    }
+}
