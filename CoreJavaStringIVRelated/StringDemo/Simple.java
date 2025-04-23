0d409b261ecb9b551faac647a3170ace7871ed78
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-04-23 18:08:59 +0530

    commit final

--- CoreJavaStringIVRelated/StringDemo/Simple.java
+++ CoreJavaStringIVRelated/StringDemo/Simple.java
@@ -1,28 +1,31 @@
-//package StringDemo;
-
 public class Simple {
     public static void main(String[] args){
-    //creating String with string literal
-    String s1="Yogesh";
-    //creating character array of string
-    char[] ch= {'K','I','R','A','N'} ;
+        // Creating String with string literal
+        String s1 = "Yogesh";
 
-    //converting char array to string
-    String s2=new String(ch);
+        // Creating character array
+        char[] ch = {'K','I','R','A','N'};
 
-    //converting String into character array
-    char[] s3=s1.toCharArray();  //String s1="Yogesh";
+        // Converting char array to String
+        String s2 = new String(ch);
 
+        // Converting String into character array
+        char[] s3 = s1.toCharArray();
+
+        // Creating Java string using new keyword
+        String s4 = new String("Satodkar");
+
+        // Printing results
+        System.out.println("String s1: " + s1);
+        System.out.println("String s2 (from char array): " + s2);
+
+        // Properly printing char array
+        System.out.print("Char array s3 (from s1): ");
+        for (char c : s3) {
+            System.out.print(c + " ");
+        }
+
+        System.out.println(); // for newline
+        System.out.println("String s4: " + s4);
     }
-    System.out.println("string is ");
-    //creating Java string using new creating
-    String s4=new String("Satodkar");
-
-    System.out.println(" string s1 :" +s1);
-    System.out.println("string s2: "+s2.toString());  //[C@63e31ee
-    System.out.println("string s3: "+s3.toString());
-    System.out.println();
-    System.out.println("string s4: "+s4);
-
-
-  }
+}
