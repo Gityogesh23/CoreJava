cbb9eace725f06afc3e02ad211d0e380dd538efd
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-05-16 23:30:32 +0530

    commit today

---
+++ Java8featurrsImplementationCodes/AboutFunctionalInterface/Folder1/ConsumerDemo.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+//Consumer<T>interface represents an operation that accepts a single input argument and
+//returns no result(void).It's useful for performing actions on
+//elements of a collection without modifying the collection itself.
+//functional interface demo
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        List<Integer>numbers=Arrays.asList(1,2,3,4,5,6);
+      //Consumer to print each number 
+    Consumer<Integer>printNumber=num->System.out.println("Numbers :" +num);
+    
+    System.out.println("Printing each number:");
+    numbers.forEach(printNumber);
+
+// Consumer to print the square of each number
+Consumer<Integer>printSquare=num -> System.out.println("Square of " +num+ (num*num));    
+
+System.out.println("/Printing the square of each number: ");
+numbers.forEach(printSquare);
+//chaining Consumers using andThen()
+Consumer<Integer>printAndThenSquare=printNumber.andThen(printSquare);
+     
+System.out.println("\nPrinting number and then its square:");
+        numbers.forEach(printAndThenSquare);
+    }
+
+}
