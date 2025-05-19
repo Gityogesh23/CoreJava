fa0b4c300fbfb566c8e8ddde8c374590c0cf5ff8
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-05-19 18:48:49 +0530

    commit second logic for it.

--- Array/Second_Highest/FindSecondHighest.java
+++ Array/Second_Highest/FindSecondHighest.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class FindSecondHighest {
+    public static void main(String[] args) {
+        Integer[] arr = {10, 20, 40, 50, 50, 30};
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int first = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < first) {
+                System.out.println("Second highest: " + arr[i]);
+                break;
+            }
+        }
+    }
+}
+
+
+/* 
 import java.util.TreeSet;
 
 public class FindSecondHighest {
@@ -16,3 +36,4 @@ public class FindSecondHighest {
 
    }
 }
+*/
