aac8746b535917ba234204c73b124950b9fde48e
Yogesh Patil <yogeshpatil1844@gmail.com>
2025-05-22 21:19:05 +0530

    commi

--- AbstractClassDemo/AbstractClassDemo_1.java
+++ AbstractClassDemo/AbstractClassDemo_1.java
@@ -1,47 +1,54 @@
-
-abstract class Shape{
+abstract class Shape {
     String color;
 
-    Shape(String color){
-        this.color=color;
+    Shape(String color) {
+        this.color = color;
     }
+
     abstract double getArea();
 
-    public void display(){
-        System.out.println("the color of "+color+ "shape");
+    public void display() {
+        System.out.println("the color of " + color + " shape");
     }
 }
-    class Circle extends Shape{
-        double radius;
-        Circle(String color,double radius){
-            super(color);
-            this.radius=radius;
-        }
-        @Override
-        double getArea() {
-            return Math.PI*radius*radius;            
-        }
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
     }
-        class Rectangle extends Shape{
-            int length,width;
-            Rectangle(String color,int length,int width){
-                super(color);
-                this.length=length;
-                this.width=width;
-            }
-            @Override
-            double getArea() {
-                return length*width;            
-            }
+
+    @Override
+    double getArea() {
+        return Math.PI * radius * radius;
     }
+}
+
+class Rectangle extends Shape {
+    int length, width;
+
+    Rectangle(String color, int length, int width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double getArea() {
+        return length * width;
+    }
+} // ✅ FIXED: Properly closed Rectangle class here
+
 public class AbstractClassDemo_1 {
     public static void main(String[] args) {
-        Circle circle=new Circle("Blue",12.00);
-        System.out.println("Area of Circle is: "+circle.getArea());
-        circle.display(); // Optional - show color info
-        Rectangle rectangle=new Rectangle("Red",12,12);
-        System.out.println("Area of Circle is: "+rectangle.getArea());
-        rectangle.display(); // Optional - show color info
-        
+        Circle circle = new Circle("Blue", 12.00);
+        System.out.println("Area of Circle is: " + circle.getArea());
+        circle.display();
+
+        Rectangle rectangle = new Rectangle("Red", 12, 12);
+        System.out.println("Area of Rectangle is: " + rectangle.getArea());
+        rectangle.display();
     }
-  }
+}
