9528c07c411bc88751450f6478167f28c89b6ab5
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-04-07 18:16:22 +0530

    commit the changes

--- LeapYearCheck/LeapYear.java
+++ LeapYearCheck/LeapYear.java
@@ -6,6 +6,10 @@ public class LeapYear {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Year: ");
         int year=sc.nextInt();
+        try{
+        if(year<=0){
+            System.out.println("Kindly enter positive integer number except zero");
+        }
 
         //if( year %400==0 ){  OR
             if(year%400==0 || (year%4==0 && year%100 !=0)){
@@ -15,5 +19,8 @@ public class LeapYear {
         //(year%4==0 && year%100 !=0 )
             System.out.println("Not a leap year");
         }
+      }catch(Exception e){
+        System.out.println("Input Mismatch: plz enter vlid input i.e number except 0");
+      }
     }
 }
