2634a6c1774384e80c89ae03dfdeda86818f9c42
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-04-13 19:13:57 +0530

    just making small change

--- MiscellaneousExamples/folder_1/ArraysExample1.java
+++ MiscellaneousExamples/folder_1/ArraysExample1.java
@@ -4,7 +4,7 @@ public class ArraysExample1 {
     public static void main(String[] args){
        // List<Integer>numbers=Arrays.asList(1,2,3);
 //Arrays.asList(T...a);creates immutable list 
-//i.e.In which we r able to set an element inside existing list but not able to add elements.
+//i.e.In which we'r able to set an element inside existing list but not able to add elements.
 //instead you shuld go with List.of()       
 List<Integer>numbers=List.of(1,2,3,4,5,6,7,8,9);
      //List.of(T ...e);==>does't allowing to add or set the element into list.   
