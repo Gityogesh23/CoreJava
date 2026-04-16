7c01c51b5e8a6da0ed00abddc1710ddfafbe6c17
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-04-16 18:31:16 +0530

    commit the code

---
+++ LCM/Lcm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/*
+LCM(Least common Multiple) ? 
+The least number which is exactly divisible by each one of the given numbers is called 
+their L.C.M .
+Formula:
+H.C.F. and L.C.M. of Fractions:
+
+    1. H.C.F. =	H.C.F. of Numerators / L.C.M. of Denominators
+
+    2. L.C.M. =	L.C.M. of Numerators / H.C.F. of Denominators
+
+# First need to undestand-->
+Factors and Multiples:
+If number a divided another number b exactly, we say that a is a factor of b.
+
+In this case, b is called a multiple of a.
+ */
+class Lcm{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number whose LCM to be Calculate: ");
+        int num=sc.nextInt();
+    }
+}
