1622b9072056cbc845346dc6fdeeb15df37992c7
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-02 19:47:00 +0530

    commit the code

--- Factorialnumber/Factorial.java
+++ Factorialnumber/Factorial.java
@@ -135,12 +135,3 @@ import java.util.Scanner;
 <<<<<<< HEAD
 
 */
-/*
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt update
-sudo apt install jenkins
-*/
