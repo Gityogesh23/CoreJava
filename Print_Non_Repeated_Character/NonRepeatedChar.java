0c9c5aa31efe6831e5ef2ab609dd2d5b376bbd42
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-15 20:21:30 +0530

    commit the code .

---
+++ Print_Non_Repeated_Character/NonRepeatedChar.java
@@ -0,0 +1,23 @@
+
+public class NonRepeatedChar {
+    public static void main(String[] args) {
+        String str = "aabbcdde";
+
+        for(char c : str.toCharArray()){
+
+        if(str.indexOf(c) == str.lastIndexOf(c)){
+        System.out.println(c);
+        break;
+    }
+}
+    }
+}
+
+/*
+indexOf(int ch): Returns the index of the first occurrence of the 
+specified character (Unicode value).
+
+indexOf(int ch, int fromIndex): Searches for a character starting from a specific index.
+indexOf(String str): Returns the index of the first occurrence of the specified substring.
+indexOf(String str, int fromIndex): Searches for a substring starting from a specific index
+*/
