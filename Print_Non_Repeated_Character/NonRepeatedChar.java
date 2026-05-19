39202d5082d257cb67db4317afe5d2facba05acc
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-19 20:38:16 +0530

    commit the code

--- Print_Non_Repeated_Character/NonRepeatedChar.java
+++ Print_Non_Repeated_Character/NonRepeatedChar.java
@@ -20,4 +20,11 @@ specified character (Unicode value).
 indexOf(int ch, int fromIndex): Searches for a character starting from a specific index.
 indexOf(String str): Returns the index of the first occurrence of the specified substring.
 indexOf(String str, int fromIndex): Searches for a substring starting from a specific index
+
+
+----------------------------------------
+indexOf(): Finds the first occurrence of the specified value, searching from the beginning 
+of the string/list.
+lastIndexOf(): Finds the last occurrence of the specified value, effectively searching 
+backwards from the end
 */
