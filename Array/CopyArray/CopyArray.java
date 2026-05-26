76a7be0885d198da91fd016a2c8c747e9b51ef78
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-05-26 19:41:23 +0530

    commit the code.

--- Array/CopyArray/CopyArray.java
+++ Array/CopyArray/CopyArray.java
@@ -75,31 +75,17 @@ public class CopyArray{
 }    
 */
 /*
-Hard Coded:
-public class CopyArray{    
-    public static void main(String[] args) {        
-             //Initialize array     
-        int [] arr1 = new int [] {1, 2, 3, 4, 5};     
-         //Create another array arr2 with size of arr1    
-        int arr2[] = new int[arr1.length];    
-        //Copying all elements of one array into another    
-        for (int i = 0; i < arr1.length; i++) {     
-            arr2[i] = arr1[i];     
-        }      
-         //Displaying elements of array arr1     
-        System.out.println("Elements of original array: ");    
-        for (int i = 0; i < arr1.length; i++) {     
-           System.out.print(arr1[i] + " ");    
-        }     
-            
-        System.out.println();    
-            
-        //Displaying elements of array arr2     
-        System.out.println("Elements of new array: ");    
-        for (int i = 0; i < arr2.length; i++) {     
-           System.out.print(arr2[i] + " ");    
-        }     
-    }    
-}    
+Arrays are used to store multiple values in a single variable, instead of declaring 
+separate variables for each value.
+
+To declare an array, define the variable type with square brackets [ ] :
+eg:
+int[]arr ;
+
+We have now declared a variable that holds an array of strings. To insert values to it, 
+you can place the values in a comma-separated list, inside curly braces { }:
+
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
 */
 
