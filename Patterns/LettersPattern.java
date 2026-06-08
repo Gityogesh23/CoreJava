1d6fd8dba9427202915c7dc55a994bd8c129c04b
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-06-08 20:12:58 +0530

    commit the code.

---
+++ Patterns/LettersPattern.java
@@ -0,0 +1,26 @@
+/*
+A
+A B
+A B C
+A B C D
+
+*/
+import java.util.Scanner;
+public class LettersPattern {
+    public static void main(String[] args) {
+          Scanner sc=new Scanner(System.in);
+          //Represents rows till which output pattern is expected.
+          System.out.println("Enter the number :");
+          int n=sc.nextInt();
+          for(int i=1;i<n;i++){
+            char ch='A';
+            for(int j=1;j<=i;j++){
+                System.out.print(" "+ch);
+                ch++;
+            }
+            System.out.println();
+          }
+
+          
+    }
+}
