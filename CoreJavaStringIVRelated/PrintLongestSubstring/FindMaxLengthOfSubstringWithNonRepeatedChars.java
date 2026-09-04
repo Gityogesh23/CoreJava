fd50598ae002d9480d4ca580126d356ca34405f1
Yogesh Patil <yogeshpatil1844@gmail.com>
2026-09-04 11:28:40 +0530

    commit the chnages which are made.

--- CoreJavaStringIVRelated/PrintLongestSubstring/FindMaxLengthOfSubstringWithNonRepeatedChars.java
+++ CoreJavaStringIVRelated/PrintLongestSubstring/FindMaxLengthOfSubstringWithNonRepeatedChars.java
@@ -1,47 +1,47 @@
 // package CoreJavaStringIVRelated.PrintLongestSubstring;
 
 
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
 
-public class FindMaxLengthOfSubstringWithNonRepeatedChars {
-	public static int calLengthOfSubstring(String s){
-		if(s==null && s.isEmpty()) {
-			return 0;
-		}
-		int maxLength=0;
-		int Left=0;
-		Set<Character>set=new HashSet<>();
+// public class FindMaxLengthOfSubstringWithNonRepeatedChars {
+// 	public static int calLengthOfSubstring(String s){
+// 		if(s==null && s.isEmpty()) {
+// 			return 0;
+// 		}
+// 		int maxLength=0;
+// 		int Left=0;
+// 		Set<Character>set=new HashSet<>();
 		
-		for(int Right=0;Right<s.length();Right++) {
-			//to remove already contained character element inside set.
-			while(set.contains(s.charAt(Right))) {
-				set.remove(s.charAt(Left));
-				Left++;
-			}
+// 		for(int Right=0;Right<s.length();Right++) {
+// 			//to remove already contained character element inside set.
+// 			while(set.contains(s.charAt(Right))) {
+// 				set.remove(s.charAt(Left));
+// 				Left++;
+// 			}
 			
-			//Add if not contains by set<V>
-			set.add(s.charAt(Right));
-			maxLength=Math.max(maxLength,Right-Left+1);
-		}
+// 			//Add if not contains by set<V>
+// 			set.add(s.charAt(Right));
+// 			maxLength=Math.max(maxLength,Right-Left+1);
+// 		}
 		
-		return maxLength;
-	}
-	public static void main(String[] args) {
+// 		return maxLength;
+// 	}
+// 	public static void main(String[] args) {
 		
-		// TODO Auto-generated method stub
-		String input="abcabcbb";
-		System.out.println("Longest substring length would be: " +calLengthOfSubstring(input)); //Longest substring length would be: 3
-	// Output: 3 ( because the substring is "abc")
-	}
+// 		// TODO Auto-generated method stub
+// 		String input="abcabcbb";
+// 		System.out.println("Longest substring length would be: " +calLengthOfSubstring(input)); //Longest substring length would be: 3
+// 	// Output: 3 ( because the substring is "abc")
+// 	}
 
-}
-/*
+// }
+/* */
 import java.util.Scanner;
 import java.util.Set;
 import java.util.HashSet;
 
-class Main{
+class FindMaxLengthOfSubstringWithNonRepeatedChars{
     public static void getSubstring(String str){
         if(str==null){
           throw new IllegalArgumentException("string shoud have charactes as input.");
@@ -78,7 +78,6 @@ class Main{
     //   System.out.println("unique substring becomes :"+result);
     }
 }
-*/
 
 /* Find an unique substring from input string*/
 
